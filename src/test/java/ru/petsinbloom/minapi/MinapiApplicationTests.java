7dac993a00a6a427237dbe28d902b7f34013c27a
Eric Young <eric.youngheart@gmail.com>
2025-07-29 12:08:05 +0300

    Исходный коммит после генерации шаблона проекта Spring и копирования его в выбранную директорию. Исходная точка проекта.

---
+++ src/test/java/ru/petsinbloom/minapi/MinapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petsinbloom.minapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
